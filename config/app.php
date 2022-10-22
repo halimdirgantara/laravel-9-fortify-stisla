d7ff99676120a70507bde88a8101457d0371bf1b
halimdirgantara <halimdirgantara@gmail.com>
2022-10-22 18:34:01 +0700

    feat: spatie roles and permissions

--- config/app.php
+++ config/app.php
@@ -186,6 +186,7 @@ return [
          * Package Service Providers...
          */
         App\Providers\FortifyServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
 
         /*
