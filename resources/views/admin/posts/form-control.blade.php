314235cf3de49d2d1ad2e183e65621834cfd6ab4
halimdirgantara <halimdirgantara@gmail.com>
2022-11-03 21:32:22 +0700

    feat: Post view CRUD

---
+++ resources/views/admin/posts/form-control.blade.php
@@ -0,0 +1,40 @@
+<div class="form-group">
+    <label for="name">Name</label>
+    <input id="name" type="text" class="form-control" required name="name" value="{{ old('name') ?? $post->name }}" autofocus>
+</div>
+@error('name')
+    <div class="alert alert-danger mt-2">
+        {{ $message }}
+    </div>
+@enderror
+<div class="form-group">
+    <label for="name">Content</label>
+    <input id="name" type="text" class="form-control" required name="name" value="{{ old('name') ?? $post->name }}" >
+</div>
+@error('name')
+    <div class="alert alert-danger mt-2">
+        {{ $message }}
+    </div>
+@enderror
+<div class="form-group">
+    <label for="category">Category</label>
+    <br>
+    <select name="category" id="category" class="form-control select2">
+        <option value="" selected>--Select Category --</option>
+        @foreach ($categories as $category)
+            <option {{ $post->categories->isEmpty() ?  '' : ($post->rolecategoriess[0]->name == $category->name ? 'selected' : '') }} value="{{ $category->id }}">{{ $category->name }} </option>
+        @endforeach
+    </select>
+    @error('permission')
+        <div class="mt-2 text-danger">{{ $message }}</div>
+    @enderror
+</div>
+<div class="form-group">
+    <label for="name">Image</label>
+    <input id="name" type="file" class="form-control" required name="name" value="{{ old('name') ?? $post->name }}" >
+</div>
+@error('name')
+    <div class="alert alert-danger mt-2">
+        {{ $message }}
+    </div>
+@enderror
