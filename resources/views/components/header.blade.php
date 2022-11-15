149edd4233e1dee37018eca0d1284a0c35a0d09f
halimdirgantara <halimdirgantara@gmail.com>
2022-11-15 21:47:44 +0700

    feat: frontend layout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -9,7 +9,7 @@
         <li class="dropdown"><a href="#" data-toggle="dropdown"
                 class="nav-link dropdown-toggle nav-link-lg nav-link-user">
                 <img alt="image" src="{{ asset('img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
-                <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }}</div>
+                <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::check() ? Auth::user()->name : 'Guest' }}</div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
                 <a href="{{ route('profile.edit') }}" class="dropdown-item has-icon">
